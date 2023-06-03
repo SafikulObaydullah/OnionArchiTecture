d8658dd085358a7f5bfe208e933e2cc23c08016c
obaydullah <obaydullah599@gmail.com>
2023-06-03 22:19:20 +0600

    Annotaition added

--- DomainLayer/Models/Request/CustomerInfo.cs
+++ DomainLayer/Models/Request/CustomerInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DomainLayer.Models
@@ -7,7 +9,12 @@ namespace DomainLayer.Models
    public class CustomerInfo 
    {
         public int Id { get; set; }
+        [StringLength(50)]
         public string CustomerName { get; set; }
+         [Required]
+         [StringLength(50)]
+         [Column("PurchasesProduct")]
+         [Display(Name = "Purchases Product")]
         public string PurchasesProduct { get; set; }
         public string PaymentType { get; set; }
    }
