017f6986a81d1420df691af86dec412f93f3e959
Md. Obaydullah <Md. Obaydullah@DESKTOP-MV6UQ21>
2023-06-05 08:47:26 +0600

    addd

--- MVCClient/Controllers/HomeController.cs
+++ MVCClient/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace MVCClient.Controllers
       {
          return View();
       }
-
+      public IActionResult RemoveJquery()
+      {
+         return View();
+      }
       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
       public IActionResult Error()
       {
