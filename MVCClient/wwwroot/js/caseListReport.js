c02319d235883a6616f31eabcafc7f7b8392bd00
Md. Obaydullah <Md. Obaydullah@DESKTOP-MV6UQ21>
2023-06-06 17:20:12 +0600

    rpt added

--- MVCClient/wwwroot/js/caseListReport.js
+++ MVCClient/wwwroot/js/caseListReport.js
@@ -1,7 +1,4 @@
-﻿
-var GridData = []
-
-
+﻿var GridData = []
 function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -12,7 +9,8 @@ function getUrlParameter(sParam) {
         }
     }
 }
-var CaseNo = getUrlParameter('CaseId');
+
+var CaseNo = getUrlParameter('caseNo');
 var Year = getUrlParameter('year');
 var CourtTypeID = getUrlParameter('CourtTypeId');
 var CaseCategoryID = getUrlParameter('CaseCategory');
@@ -26,32 +24,26 @@ var IssueDateTo = getUrlParameter('issueDateTo');
 $(document).ready(function () {
     CaseNo == undefined ? 0 : CaseNo;
     CaseReportDetails();
-
 });
 
-
 function CaseReportDetails() {
-    var o = new Object();
-    o.CaseNo = CaseNo;
-    o.Year = Year;
-    o.CourtTypeId = CourtTypeID;
-    o.CaseCategoryID = CaseCategoryID;
-    o.CaseTypeID = CaseTypeID;
-    o.CourtID = CourtID;
-    o.ConcernedPersonID = ConcernedPersonID;
-    o.ConcernedOfficeID = ConcernedOfficeID;
-    o.IssueDateFrom = IssueDateFrom;
-    o.IssueDateTo = IssueDateTo;
-    console.log(o)
-    /*blockUI()*/
+    var obj = new Object();
+    obj.CaseNo = CaseNo;
+    obj.Year = Year;
+    obj.CourtTypeId = CourtTypeID;
+    obj.CaseCategoryID = CaseCategoryID;
+    obj.CaseTypeID = CaseTypeID;
+    obj.CourtID = CourtID;
+    obj.ConcernedPersonID = ConcernedPersonID;
+    obj.ConcernedOfficeID = ConcernedOfficeID;
+    obj.IssueDateFrom = IssueDateFrom;
+    obj.IssueDateTo = IssueDateTo;
     $.ajax({
         url: "/Report/ReportSearchResult",
         method: "GET",
         dataType: "json",
-        data: o,
+        data: obj,
         success: function (data) {
-            console.log(data);
-            /*unblockUI()*/
             GridData = data;
             if (GridData.length > 0) {
                 BindData();
@@ -62,18 +54,30 @@ function CaseReportDetails() {
         }
     })
 }
+
 function BindData() {
     var result = GridData;
     $("#caseListReport tbody").empty();
     var sl = 1;
     for (var i = 0; i < result.length; i++) {
-        var html = "<tr><td style='text-align:center; ' >" + sl + "</td>" +
-            "<td style='width:10%;text-align:left;' > " + result[i].nameE + "</td> " +
+        var tempIssueDate = result[i].issueDate == '0001-01-01T00:00:00' || result[i].issueDate == '1900-01-01T00:00:00' ? ' ' : moment(result[i].issueDate).format('MMM Do YYYY');
+        var tempActivityDate = result[i].activityDate == '0001-01-01T00:00:00' || result[i].activityDate == '1900-01-01T00:00:00' ? ' ' : moment(result[i].activityDate).format('MMM Do YYYY');
+        var tempHearingDate = result[i].nextHearingDate == '0001-01-01T00:00:00' || result[i].nextHearingDate == '1900-01-01T00:00:00' ? ' ' : moment(result[i].nextHearingDate).format('MMM Do YYYY');
+        var html = "<tr><td id='slNo' style='text-align:center;vertical-align:inherit;' onclick='Report(" + result[i].id + ")' >" + sl + "</td>" +
             "<td style='width:10%;text-align:left;'>" + result[i].caseNo + "</td>" +
+            "<td style='width:10%;text-align:left;' > " + result[i].nameE + "</td> " +
             " <td style='width:30%;text-align:left;'> " + result[i].caseSubject + "</td>" +
-            " <td style='width:40%;text-align:left;word-wrap: break-word;overflow-wrap:anywhere' >" + result[i].caseDescription + "</td>" +
+            " <td style='width:10%;text-align:left;'> " + result[i].casePriority + "</td>" +
+            " <td style='width:10%;text-align:left;'> " + tempIssueDate + "</td>" +
+            " <td style='width:10%;text-align:left;'> " + tempActivityDate + "</td>" +
+            " <td style='width:10%;text-align:left;'> " + tempHearingDate + "</td>" +
             "</tr>";
         $("#caseListReport tbody").append(html)
         sl = sl + 1;
     }
+}
+
+function Report(id) {
+    var url = '/Report/Index?ID=' + id;
+    window.open(url, '_blank');
 }
